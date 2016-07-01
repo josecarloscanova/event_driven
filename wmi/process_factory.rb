10e28f4113c79bc09513c082de25a8f5296e3fc4
josecarloscanova <jose.carlos.canova@gmail.com>
2016-07-01 16:33:06 -0300

    Process
    Altered Factory Objects. added Service and Process "rojos".

--- wmi/process_factory.rb
+++ wmi/process_factory.rb
@@ -1,16 +1,16 @@
-class SimpleProcessFactory
+class ProcessFactory
 
   def initialize
   end
     
-  def SimpleProcessFactory.simple_process_with content
+  def ProcessFactory.simple_process_with content
       verify_record content
       @@process.new(content['osname'] , content['caption'] , content['csname'] , content['processid'] , content['parentprocessid'] , content['description'] )
   end
   
   private 
   
-  def SimpleProcessFactory.verify_record content
+  def ProcessFactory.verify_record content
     raise ArgumentError , 'Wrong number of arguments' unless @@process.members.select{|k,v|
            content.respond_to?k
            }.length <= @@process.members.length
