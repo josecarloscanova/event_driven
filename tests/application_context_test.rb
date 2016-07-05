57cf180ae465ec7d9ff1ce742cc29673a2c82660
josecarloscanova <jose.carlos.canova@gmail.com>
2016-07-04 23:40:07 -0300

    fixed excessive tapping

--- tests/application_context_test.rb
+++ tests/application_context_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative '../context/wmi_application_context'
+require_relative '../context/web_application_context'
 
 class ApplicationContextTest < Minitest::Test
   
@@ -23,6 +24,15 @@ class ApplicationContextTest < Minitest::Test
   end
 
 
+  def test_application_context_initialization_1
+       Nanotek::WebApplicationContext.new{
+                       Nanotek::WebApplicationContext.configure_application_context
+                             assert_equal($wmi_application_context_loaded , true , "Wmi Application Context not Initialized")
+                  }.join
+       puts "Wmi application context #{$wmi_application_context_loaded}"
+     true
+   end
+  
 end
 
 #Nanotek::ApplicationContext.configure_application_context
