6ee29cbbf5e6740f0bc39912f91d6a4254507c3a
Joselito Barbacena <jose.carlos.canova@gmail.com>
2016-07-07 01:36:40 -0300

    updating tests

---
+++ tests/win32_system_bios_test.rb
@@ -0,0 +1,43 @@
+require 'yaml'
+require 'psych'
+require 'minitest/autorun'
+require_relative '../wmi/wmi_class_definition'
+require_relative '../wmi/wmi_service'
+require_relative '../wmi/wmi_service_configurator'
+require_relative '../wmi/wmi_configuration_factory'
+require_relative '../wmi/wmi_result_class_transformer'
+require_relative '../wmi/wmi_class_factory'
+
+module Nanotek 
+
+    class  Win32SystemBiosTest < Minitest::Test
+    #  WmiServiceConfigurator
+      def test_system_32
+        class_loaded = YamlUnMarshaller.new.unmarshall
+        wcf = Nanotek::WmiClassFactory.new(class_loaded["Win32_SystemBIOS"])
+        wmi_service = Nanotek::WmiService.new({:service => class_loaded["Win32_SystemBIOS"].name , location => class_loaded["Win32_SystemBIOS"].path})
+        win32_system_instances =  wmi_service.instances_of(class_loaded["Win32_SystemBIOS"].name)
+        result =  wmi_service.get_instances_of({:class => class_loaded["Win32_SystemBIOS"].name , :wcf => wcf ,  :result => win32_system_instances})
+        puts result.flatten.select {|k| !k[0].nil?}
+        end
+      
+    end
+
+    
+    class YamlUnMarshaller
+      
+      
+        attr_reader(:class_loaded)
+      
+          def initialize 
+          end
+          
+          def unmarshall
+            parser = Psych::Parser.new
+            @@class_loaded = YAML.load(IO.read("C:/cygwin64/home/user/event_driven/wmi/classes/yaml/Win32_SystemBIOS.yml"))
+          end  
+          
+          
+    end  
+    
+end
